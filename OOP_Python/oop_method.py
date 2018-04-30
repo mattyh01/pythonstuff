690a5d1753731f1a65493f05ae633ea98156343e
User One <User@mail.com>
2018-04-30 15:58:32 +0100

    Video 4 of OOP done, lots more info in oop_method.py

--- OOP_Python/oop_method.py
+++ OOP_Python/oop_method.py
@@ -50,8 +50,54 @@ class Employee:
             return "No!"
         return "Yes."
 
+class Developer(Employee):
+    def __init__(self, emp_id, fname, lname, pay, language):
+        Employee.__init__(self, emp_id, fname, lname, pay) #Initialise from parent class
+        self.language = language
+    raise_amount = 1.24 #Here we set a different raise amount for the Class Develoepr
+
+
+class Manager(Employee):
+    def __init__(self, emp_id, fname, lname, pay, band, employees=None):
+        Employee.__init__(self, emp_id, fname, lname, pay)
+        self.band = band
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+    raise_amount = 1.70
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def add_emp(self, emp):
+        if emp  in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fname())
+
 emp_1 = Employee(Employee.id_inc, "Matt", "Holmes", 30000)
 emp_2 = Employee(Employee.id_inc, "Test", "User", 40000)
+
+dev_1 = Developer(Employee.id_inc, "Junior", "Developer", 24000, "C++ ")
+dev_2 = Developer(Employee.id_inc, "Senior", "Tester", 34880, "Java")
+
+mgr_1 = Manager(Employee.id_inc, "Bastard", "Manager", 24000, 8, [emp_1])
+
+print mgr_1.fname + " " + mgr_1.lname +  " is at a band " + str(mgr_1.band)
+
+#print help(Developer) #Good for looking up what the class contains
+print(isinstance(mgr_1, Manager)) # Useful to see if first arg is an instance of the second arg
+print(issubclass(Developer, Employee))
+print dev_1.pay
+
+print dev_1.language
+print dev_2.language
+
+
 
 emp_str_1 = ('99-Alex-McCarthy-76500')
 comma_str_2 = ('44,Fraser,Forster,81250')
@@ -60,26 +106,28 @@ comma_str_2 = ('44,Fraser,Forster,81250')
 new_emp_1 = Employee.from_string(emp_str_1)
 new_emp_2 = Employee.from_commasep(comma_str_2)
 
-print Employee.hello(new_emp_2)
-
 date = datetime.date(2001, 11, 5)
-print "Is today a working day?"
-print ((Employee.isworkday(date)))
 
-print emp_1.raise_amount
-Employee.apply_payrise(emp_1) # Here we call the class first, then the
+#print Employee.hello(new_emp_2)
+
+
+#print "Is today a working day?"
+#print ((Employee.isworkday(date)))
+
+#print emp_1.raise_amount
+#Employee.apply_payrise(emp_1) # Here we call the class first, then the
                             #function we want to execute, along with the
                             #object we want the function to execute on
                             #this is the standard way of performing this'''
 
-emp_1.apply_payrise         #Here we call the function apply_payrise, which calculates pay * raise_amount
+#emp_1.apply_payrise         #Here we call the function apply_payrise, which calculates pay * raise_amount
 
-print emp_1.pay                        #print Employee.__dict__ # Prints out the attributes of the Class/Object'''
+#print emp_1.pay                        #print Employee.__dict__ # Prints out the attributes of the Class/Object'''
 
-Employee.set_payrise_cls(1.07) #This is done via the class variable
+#Employee.set_payrise_cls(1.07) #This is done via the class variable
 
                             ##Here the object (emp1) is specfically set so that'''
-print Employee.raise_amount  #Print out the amount to be raised
+#print Employee.raise_amount  #Print out the amount to be raised
 
 
 #print emp_1.pay
