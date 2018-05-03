b2ba9960c21c44ca66b880560b95f6163d490090
Matt <mholmes01@btinternet.com>
2018-05-03 11:31:24 +0100

    Flask education

---
+++ Flask/starter_flask.py
@@ -0,0 +1,24 @@
+from flask import Flask, flash, redirect, render_template, session, abort
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+#Different routes below as defined
+#by the decorator @app.route()
+
+@app.route("/aboutme")
+def aboutme():
+    return "About me page"
+
+@app.route("/championships/<string:name>/")
+def championships(name):
+    return render_template(
+        'test.html', name=name)
+
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001)
