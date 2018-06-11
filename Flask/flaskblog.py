842a2a1190766fbdbbc4bdec01746924cddbcb79
Matt <mholmes01@btinternet.com>
2018-06-11 09:53:52 +0100

    Added starter DB stuff

--- Flask/flaskblog.py
+++ Flask/flaskblog.py
@@ -1,8 +1,38 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite//site.db'
+db = SQLAlchemy(app)
+
+#DB info here
+
+class User(db.Model):
+    id = db.column(db.Integer, primary key)
+    username = db.column(db.String(20), unique=True, nullable=False)
+    email = db.column(db.String(120), unique=True, nullable=False)
+    image_file = db.column(db.String(20)) nullable=False, default='default.jpg')
+    password = db.column(db.String(60)), nullable=False)
+
+#Repr how the object is printed out
+
+    def __repr__(self):
+        return f"User('{self.username}'), '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.column(db.Integer, primary key)
+    title = db.column(db.String(100), nullable=False)
+    date = db.column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.column(db.Text, nullable=False)
+
+def __repr__(self):
+    return f"Post('{self.title}'), '{self.date_posted}')"
+
+
 
 posts = [
     {
@@ -53,5 +83,5 @@ def login():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')	
+    app.run(host='0.0.0.0')
     app.run(debug=True)
